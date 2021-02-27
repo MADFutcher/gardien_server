06b9a2cce650301a64f7c8d6dde643501fcea9bb
MADFutcher <mikalob247@gmail.com>
2021-02-27 15:48:33 +0100

    adjusted variable name

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ require('./configs/passport');
 
 
 mongoose
-  .connect(process.env.MONGO_URL, {useNewUrlParser: true})
+  .connect(process.env.MONGO_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
