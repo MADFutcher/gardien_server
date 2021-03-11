5d82b3074130003e8b1edf73b04724a91f4b2fc3
MADFutcher <mikalob247@gmail.com>
2021-03-11 21:12:05 +0100

    Adjusted variable

--- ping.js
+++ ping.js
@@ -1,5 +1,5 @@
 var ping = require('ping');
-var hosts = [ENV['PING_URL']];
+var hosts = [process.env.PING_URL];
 hosts.forEach(function(host){
     ping.sys.probe(host, function(isAlive){
         var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
